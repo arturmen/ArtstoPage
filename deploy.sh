a76249bdb6d040e425baa1192c0446c3cb0bd8b0
Wiktor Muron <wiktor.muron@nauto.com>
2018-10-04 00:38:12 +0200

    Deployment to GCP

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# gsutil cp ArtstoPage/* gs://artsto-preview
+# gsutil rsync -d -r ArtstoPage gs://artsto-preview
+gsutil -m rsync -d -r -x '\.git.*' ArtstoPage gs://artsto-preview
+
+# setup
+# set main page as index.html
+# gsutil web set -m index.html -e 404.html gs://artsto-preview
+
+# provide correct permissions to all the users
+# gsutil iam ch allUsers:objectViewer gs://artsto-preview
+
+# preview page at the link below
+# http://storage.googleapis.com/artsto-preview/index.html
+
