c1b12527131bfd9c6355e314b73c0ecb7607ba18
Wiktor Muron <wiktor.muron@nauto.com>
2018-10-08 01:04:18 +0200

    Refactored utils js

---
+++ js/utils.js
@@ -0,0 +1,15 @@
+
+function sanitazeName(str) {
+    return str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+}
+
+function filterifyName(str) {
+    return "pf-" + sanitazeName(str)
+}
+
+function dictToList(dict) {
+    return Object.keys(dict).map(function (key) {
+        return dict[key];
+    });
+}
+
