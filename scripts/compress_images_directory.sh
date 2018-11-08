f139c45fcd66cb2fb126aa06f817732b5214a256
Wiktor Muron <wiktor.muron@nauto.com>
2018-11-08 20:23:06 +0100

    Support miniatures

--- scripts/compress_images_directory.sh
+++ scripts/compress_images_directory.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+# run from images_work/originals
+
 args=("$@")
 dst=${args[0]}
 
 tmp_resized_dir=/tmp/resized
+tmp_miniatures_dir=/tmp/miniatures
 
 # step 1 - resizing and compression
-rm -rf ${tmp_resized_dir}
-mkdir -p ${tmp_resized_dir}
-mogrify -path ${tmp_resized_dir} -filter lanczos2 -resize '1000x1000>' -quality 90 *.jpg
-ls ${tmp_resized_dir}
+# rm -rf ${tmp_resized_dir}
+# mkdir -p ${tmp_resized_dir}
+# mogrify -path ${tmp_resized_dir} -filter lanczos2 -resize '1000x1000>' -quality 90 *.jpg
+# ls ${tmp_resized_dir}
 
-# step 2 - image optimization
-img_size=250k
+# step 2 - add miniatures
+rm -rf ${tmp_miniatures_dir}
+mkdir -p ${tmp_miniatures_dir}
+mogrify -path ${tmp_miniatures_dir} -filter lanczos2 -resize 'x100>' -quality 90 *.jpg
+ls ${tmp_miniatures_dir}
 
-mkdir -p $dst
-jpegoptim ${tmp_resized_dir}/*.jpg --size=$img_size --dest=$dst
-jpegoptim ${tmp_resized_dir}/*.png --size=$img_size --dest=$dst
+# step 3 - image optimization for large, resized images
+# img_size=250k
+
+# mkdir -p $dst
+# jpegoptim ${tmp_resized_dir}/*.jpg --size=$img_size --dest=$dst
+# jpegoptim ${tmp_resized_dir}/*.png --size=$img_size --dest=$dst
